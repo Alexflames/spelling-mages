8c97285b891a01888cd1e004413f0b4e967071a2
kamalist <kamalist5@gmail.com>
2018-11-12 07:54:32 +0400

    PROTO info panel

---
+++ Assets/GameLogic/UI/Info.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info : MonoBehaviour {
+	public GameObject infoPanel;
+	private Text header;
+	// Use this for initialization
+	void Start () {
+		int children = infoPanel.transform.childCount;
+		for (int i = 0; i < children; ++i){
+			Transform child = infoPanel.transform.GetChild (i);
+			if (child.name == "Header"){
+				header = child.gameObject.GetComponent<Text> ();
+				break;
+			}
+		}
+		infoPanel.SetActive (false);
+	}
+	
+	void SetInfo (string name){
+		infoPanel.SetActive(true);
+		header.text = name;
+	}
+}
