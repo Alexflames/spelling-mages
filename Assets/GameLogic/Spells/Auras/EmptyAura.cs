cc8ee44cfeff3faf11d23e2b3068e6859d043191
Sadpotato <alexprgrigoriev@gmail.com>
2018-12-26 16:39:35 +0400

    project .net change to 4.x.x and aura prototype

---
+++ Assets/GameLogic/Spells/Auras/EmptyAura.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyAura : IAura {
+    public string name { get; }
+	public void CastReaction() {    }	
+}
---
+++ Assets/GameLogic/Spells/Auras/IAura.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAura
+{
+    string name { get; }
+    void CastReaction();
+    // void HpLowerReaction(NetMortal hpCon);
+}
