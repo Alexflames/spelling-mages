2491265f014a15418579190add10aa0ea0275698
Sadpotato <alexprgrigoriev@gmail.com>
2019-01-02 17:30:43 +0400

    new (net)high voltage, fiery aura icon

--- Assets/GameLogic/Spells/Scripts/Network/NetHighVoltageLogic.cs
+++ Assets/GameLogic/Spells/Scripts/Network/NetHighVoltageLogic.cs
@@ -7,9 +7,10 @@ public class NetHighVoltageLogic : NetworkBehaviour
 {
     float timeToAppear;
     float timeToFade;
-    public int attackPower;
+    public int attackPower = 50;
     private double attackFactor = 1.0;
     private double speedFactor = 1.0;
+    //----------------OLD-----------------//
     public GameObject part1;
     public GameObject part2;
     private GameObject part1_1;
@@ -17,6 +18,15 @@ public class NetHighVoltageLogic : NetworkBehaviour
     private GameObject part2_1;
     private GameObject part2_2;
     private GameObject owner;
+    //--------------ALTERNATIVE--------------//
+    float defaultTTA;
+    bool TESTING = true;
+    int RNGFREQUENCY = 2;
+    public int Nobjects = 4;
+    public GameObject SubVoltageObject;
+    private List<GameObject> toContinue;
+    private List<GameObject> nextToContinue;
+    //---------------------------------------//
     //private bool greatMod = false;
     private float sF = 1.0f;//scale factor for great mod
 
@@ -66,40 +76,108 @@ public class NetHighVoltageLogic : NetworkBehaviour
     // Use this for initialization
     void Awake()
     {
-        timeToAppear = 0.25f;
-        timeToFade = 0.35f;
-        part1_1 = part1.transform.Find("HighVoltage3-1").gameObject;
-        part1_2 = part1.transform.Find("HighVoltage3-2").gameObject;
-        part2_1 = part2.transform.Find("HighVoltage3-3").gameObject;
-        part2_2 = part2.transform.Find("HighVoltage3-4").gameObject;
-        part1.SetActive(false);
-        part2.SetActive(false);
-        part1_1.SetActive(false);
-        part2_1.SetActive(false);
-        part1_2.SetActive(false);
-        part2_2.SetActive(false);
+        if (TESTING)
+        {
+            timeToFade = 0.55f;
+            defaultTTA = timeToFade / Nobjects - 0.01f;
+            timeToAppear = defaultTTA;
+
+            // prepare lists
+            toContinue = new List<GameObject>();
+            nextToContinue = new List<GameObject>();
+            toContinue.Add(gameObject);
+        }
+        else
+        {
+            timeToAppear = 0.25f;
+            timeToFade = 0.35f;
+            part1_1 = part1.transform.Find("HighVoltage3-1").gameObject;
+            part1_2 = part1.transform.Find("HighVoltage3-2").gameObject;
+            part2_1 = part2.transform.Find("HighVoltage3-3").gameObject;
+            part2_2 = part2.transform.Find("HighVoltage3-4").gameObject;
+            part1.SetActive(false);
+            part2.SetActive(false);
+            part1_1.SetActive(false);
+            part2_1.SetActive(false);
+            part1_2.SetActive(false);
+            part2_2.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeToAppear -= Time.deltaTime * (float)speedFactor;
-        timeToFade -= Time.deltaTime * (float)speedFactor;
-        if (!part1.activeSelf && timeToAppear < 0.1f)
+        if (TESTING)
         {
-            part1.SetActive(true);
-            part2.SetActive(true);
+            timeToFade -= Time.deltaTime * (float)speedFactor;
+            timeToAppear -= Time.deltaTime * (float)speedFactor;
+            // if it is time to spawn next arcs
+            if (timeToAppear < 0)
+            {
+                // For the less-chaotic appearance of lighting skip some of arcs creation on level 4 and deeper (level 1 = first arc)
+                int toSkip = toContinue.Count <= 1 ? 0 : toContinue.Count / RNGFREQUENCY;
+                //
+                foreach(var obj in toContinue)
+                {
+                    // try to skip right arc
+                    if (toSkip > 0 && Random.Range(0, (float)(toContinue.Count / toSkip)) < 1)
+                    {
+                        toSkip--;
+                        var arc = Instantiate(SubVoltageObject, obj.transform.position + obj.transform.forward - obj.transform.right * 0.25f, obj.transform.rotation, obj.transform.transform);
+                        arc.transform.Rotate(new Vector3(0, Random.Range(-25, 25), 0));
+                        nextToContinue.Add(arc);
+                    }
+                    else
+                    {
+                        // try to skip left arc
+                        if (toSkip > 0 && Random.Range(0, (float)(toContinue.Count / toSkip)) < 1)
+                        {
+                            toSkip--;
+                        }
+                        else
+                        {
+                            var arc = Instantiate(SubVoltageObject, obj.transform.position + obj.transform.forward - obj.transform.right * 0.25f, obj.transform.rotation, obj.transform.transform);
+                            arc.transform.Rotate(new Vector3(0, Random.Range(-25, 25), 0));
+                            nextToContinue.Add(arc);
+                        }
+                        var Rarc = Instantiate(SubVoltageObject, obj.transform.position + obj.transform.forward + obj.transform.right * 0.25f, obj.transform.rotation, obj.transform.transform);
+                        Rarc.transform.Rotate(new Vector3(0, Random.Range(-25, 25), 0));
+                        nextToContinue.Add(Rarc);
+                    }
+                }
+                toContinue.Clear(); // prepare list of next spawning arcs
+                foreach(var obj in nextToContinue)
+                {
+                    toContinue.Add(obj);
+                }
+                nextToContinue.Clear();
+                timeToAppear = defaultTTA;
+            }
+            else if (timeToFade < 0)
+            {
+                NetworkServer.Destroy(gameObject);
+            }
         }
-        else if (!part1_1.activeSelf && timeToAppear < 0.0f)
+        else
         {
-            part1_1.SetActive(true);
-            part2_1.SetActive(true);
-            part1_2.SetActive(true);
-            part2_2.SetActive(true);
-        }
-        else if (timeToFade < 0)
-        {
-            NetworkServer.Destroy(gameObject);
+            timeToAppear -= Time.deltaTime * (float)speedFactor;
+            timeToFade -= Time.deltaTime * (float)speedFactor;
+            if (!part1.activeSelf && timeToAppear < 0.1f)
+            {
+                part1.SetActive(true);
+                part2.SetActive(true);
+            }
+            else if (!part1_1.activeSelf && timeToAppear < 0.0f)
+            {
+                part1_1.SetActive(true);
+                part2_1.SetActive(true);
+                part1_2.SetActive(true);
+                part2_2.SetActive(true);
+            }
+            else if (timeToFade < 0)
+            {
+                NetworkServer.Destroy(gameObject);
+            }
         }
     }
 
