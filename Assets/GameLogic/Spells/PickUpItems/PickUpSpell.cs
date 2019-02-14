c7807988bcaabd485881ba1900887820961aad19
kamalist <kamalist5@gmail.com>
2019-02-14 23:32:23 +0400

    Script for items to pick up spells

---
+++ Assets/GameLogic/Spells/PickUpItems/PickUpSpell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSpell : MonoBehaviour {
+	public String spellToAdd;
+	private Type spellType;
+
+	public void Start () {
+		spellType = Type.GetType(spellToAdd);
+	}
+
+	void OnTriggerEnter (Collider collision) {
+		print("d");
+		SpellCreating sc = collision.gameObject.GetComponent<SpellCreating>();
+		if (sc == null) return;
+		collision.gameObject.AddComponent (spellType);
+		UnityEngine.Object.Destroy (this.gameObject);
+	}	
+}
